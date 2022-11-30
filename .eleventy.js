dc5ca0d0bd864d8fb5aa0f3af543ae57ba2d5958
Gordon Pedersen <death_au@hotmail.com>
2022-11-30 16:48:51 +1100

    Added 11ty to publish mds dropped by micropub to pages
    Here goes nothing...

---
+++ .eleventy.js
@@ -0,0 +1,39 @@
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addFilter("formatDate", function(value) { 
+    try{
+      const date = new Date(value)
+      if(date) return date.toISOString().replace('T', ' ').slice(0, -5)
+      else throw 'Unrecognized data format'
+    }
+    catch(e) {
+      console.log(`Could not convert "${value}"`, e)
+      return value;
+    }
+  });
+
+  eleventyConfig.addFilter("dateISOString", function(value) { 
+    try{
+      const date = new Date(value)
+      if(date) return date.toISOString()
+      else throw 'Unrecognized data format'
+    }
+    catch(e) {
+      console.log(`Could not convert "${value}"`, e)
+      return value;
+    }
+  });
+
+  eleventyConfig.addCollection("notes", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("_content/notes/*.md").reverse();
+  });
+
+  // Return your Object options:
+  return {
+    dir: {
+      input: "_content",
+      output: "pub"
+    },
+    templateFormats: ["md","html"]
+  }
+};
