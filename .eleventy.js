2e46a7f9c4103410bbc7a75ddee19a8e71b8236e
Gordon Pedersen <death_au@hotmail.com>
2022-12-08 13:19:45 +1100

    Sweeping changes and a (hopefully) proper feed page

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,7 @@ module.exports = function(eleventyConfig) {
       else throw 'Unrecognized data format'
     }
     catch(e) {
-      console.log(`Could not convert "${value}"`, e)
+      console.error(`Could not convert "${value}"`, e)
       return value;
     }
   });
@@ -19,7 +19,7 @@ module.exports = function(eleventyConfig) {
       else throw 'Unrecognized data format'
     }
     catch(e) {
-      console.log(`Could not convert "${value}"`, e)
+      console.error(`Could not convert "${value}"`, e)
       return value;
     }
   });
@@ -28,9 +28,66 @@ module.exports = function(eleventyConfig) {
     return collectionApi.getFilteredByGlob("_content/notes/*.md").reverse();
   });
 
+  eleventyConfig.addCollection("feed", function(collectionApi) {
+    return collectionApi.getAllSorted().reverse().filter(item => {
+      let showInFeed = false;
+      //if(!item.data.published) return false
+      if(item.filePathStem.startsWith('/notes/')){
+        if(item.data['like-of']) item.data.postType = "like"
+        else if(item.data['in-reply-to']) item.data.postType = "reply"
+        else if(item.data['repost-of']) item.data.postType = "boost"
+        else if(item.data['read-of']) item.data.postType = "read"
+        else if(item.data['watch-of']) item.data.postType = "watch"
+        else item.data.postType = "note";
+        showInFeed = true;
+      }
+      else if(item.filePathStem.startsWith('/articles/')){
+        item.data.postType = "article";
+        showInFeed = true;
+      }
+      else if (item.filePathStem.startsWith('/bookmarks/')){
+        item.data.postType = "bookmark";
+        showInFeed = true;
+      }
+      else if (item.filePathStem.startsWith('/likes/')){
+        item.data.postType = "like";
+        showInFeed = true;
+      }
+      else if (item.filePathStem.startsWith('/rsvp/')){
+        item.data.postType = "rsvp";
+        showInFeed = true;
+      }
+      else if (item.filePathStem.startsWith('/watched/')){
+        item.data.postType = "watched";
+        showInFeed = true;
+      }
+
+      // if(showInFeed) {
+      //   item.data.author = 
+      // }
+
+      return showInFeed;
+    });
+  });
+
+  eleventyConfig.addNunjucksShortcode("getVar", function(varString) {
+    return this.ctx[varString];
+  });
+
+  eleventyConfig.addShortcode('renderlayoutblock', function(name) {
+    return (this.page.layoutblock || {})[name] || '';
+  });
+
+  eleventyConfig.addPairedShortcode('layoutblock', function(content, name) {
+      if (!this.page.layoutblock) this.page.layoutblock = {};
+      this.page.layoutblock[name] = content;
+      return '';
+  });
+
   eleventyConfig.addPassthroughCopy({"img":"assets/img"});
   eleventyConfig.addPassthroughCopy({"js":"assets/js"});
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("CNAME");
 
   // Return your Object options:
   return {
@@ -38,6 +95,7 @@ module.exports = function(eleventyConfig) {
       input: "_content",
       output: "pub"
     },
+    htmlTemplateEngine: "njk",
     templateFormats: ["md","html"]
   }
 };
