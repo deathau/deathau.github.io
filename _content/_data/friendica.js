9b2f5cac0d9451e35f1a4b614e2ee43cb1721d45
Gordon Pedersen <death.au@death.id.au>
2024-03-25 17:42:58 +1100

    added posts page fetching from friendica

---
+++ _content/_data/friendica.js
@@ -0,0 +1,40 @@
+const EleventyFetch = require("@11ty/eleventy-fetch")
+module.exports = async function() {
+  let url = "https://monrepos.casa/api/v1/accounts/3/statuses?limit=20"
+
+  // needs basic auth, but for public posts, that doesn't need to actually be valid details?
+  const username = ''
+  const password = ''
+
+  const headers = new Headers()
+  headers.append('Authorization', 'Basic ' + Buffer.from(username + ':' + password, 'utf-8').toString('base64'))
+
+  let response = await EleventyFetch(url, {
+    fetchOptions: {
+      method: 'GET',
+      headers: headers
+    },
+    duration: "1d", // save for 1 day
+    type: "json"    // we’ll parse JSON for you
+  })
+
+  const data = response
+
+  while(response.length > 0) {
+    let lastId = data[data.length - 1].id
+    response = await EleventyFetch(url + '&max_id=' + lastId, {
+      fetchOptions: {
+        method: 'GET',
+        headers: headers
+      },
+      duration: "1d", // save for 1 day
+      type: "json"    // we’ll parse JSON for you
+    })
+    data.push(...response)
+  }
+
+  return data.filter(post => post.visibility === 'public').map(post => {
+    post.myid = new Date(post.created_at).getTime().toString(36)
+    return post
+  });
+}
